5df1ab7308faa632010436034e65656346158e24
grin-quokka <grinquokka@gmail.com>
2019-11-18 11:05:05 +0900

    :sparkles: make fake data api for main page test

--- dist/index.js
+++ dist/index.js
@@ -6,5 +6,12 @@ const app = new App_1.default().application;
 app.get('/', (req, res) => {
     res.send('Hello MEMON :x');
 });
+app.get('/main', (req, res) => {
+    const obj = {
+        moneyToPay: 500,
+        moneyToGet: 1000
+    };
+    res.send(obj);
+});
 app.listen(3000, () => console.log('http://localhost:3000'));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,13 @@ app.get('/', (req: express.Request, res: express.Response) => {
   res.send('Hello MEMON :x');
 });
 
+app.get('/main', (req: express.Request, res: express.Response) => {
+  const obj = {
+    moneyToPay: 500,
+    moneyToGet: 1000
+  };
+
+  res.send(obj);
+});
+
 app.listen(3000, () => console.log('http://localhost:3000'));
