ac329e6404a698256d53c4ceb1fc67e4bca98f2d
grin-quokka <grinquokka@gmail.com>
2019-11-28 18:29:49 +0900

    :sparkles: open api for pushtoken

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,7 +1,8 @@
 import User from '../models/User';
+import Payment from '../models/Payment';
 import * as express from 'express';
 import * as moment from 'moment-timezone';
-import Expo from 'expo-server-sdk';
+import Expo, { ExpoPushMessage } from 'expo-server-sdk';
 
 const userController = {
   signup: async (req: express.Request, res: express.Response) => {
@@ -61,6 +62,61 @@ const userController = {
       res.send(result);
     } catch (err) {
       console.log(err);
+      res.sendStatus(400);
+    }
+  },
+  sendPushToken: async (req: express.Request, res: express.Response) => {
+    try {
+      let user: User;
+      if (req.body.target === 'boss') {
+        const payment = await Payment.findOne({
+          attributes: ['bossId'],
+          raw: true,
+          where: { pricebookId: req.body.pricebookId }
+        });
+
+        user = await User.findOne({
+          raw: true,
+          where: { id: payment.bossId }
+        });
+      }
+
+      let expo = new Expo();
+      let messages: ExpoPushMessage[] = [];
+
+      if (!Expo.isExpoPushToken(user.pushtoken)) {
+        console.error(
+          `Push token ${user.pushtoken} is not a valid Expo push token`
+        );
+      }
+
+      messages.push({
+        to: user.pushtoken,
+        sound: 'default',
+        title: req.body.title,
+        body: req.body.msg,
+        data: { pricebookId: req.body.pricebookId }
+      });
+
+      let chunks = expo.chunkPushNotifications(messages);
+      (async () => {
+        for (let chunk of chunks) {
+          try {
+            let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+
+            if (ticketChunk[0].status === 'ok') {
+              res.sendStatus(200);
+            } else {
+              res.sendStatus(400);
+            }
+          } catch (error) {
+            console.error(error);
+            res.sendStatus(400);
+          }
+        }
+      })();
+    } catch (error) {
+      console.log(error);
       res.sendStatus(400);
     }
   }
